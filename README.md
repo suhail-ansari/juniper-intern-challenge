704ca498cf5b634e4b14f29ef834568771d47d4c
Suhail Ansari <suhail.ism@gmail.com>
2017-01-29 19:47:47 -0800

    removed angular router dependency from karma. added README. moved angular bootstrap dependency to devDependencies

---
+++ README.md
@@ -0,0 +1,17 @@
+#Juniper Networks Internship Coding Challenge
+
+## Run App
+To run the App
+
+    git clone git@github.com:suhail-ansari/juniper-intern-challenge.git
+    cd juniper-intern-challenge.git
+    python -m SimpleHTTPServer
+
+then open the web page in your browser using this address (http:/127.0.0.1:8000)[http://127.0.0.1]
+
+## Run Tests
+To run the Tests
+
+    npm install -g karma-cli
+    npm install
+    karma start
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,6 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       './node_modules/angular/angular.js',
-      './node_modules/angular-ui-router/release/angular-ui-router.js',
       './node_modules/angular-mocks/angular-mocks.js',
       './node_modules/angular-bootstrap-npm/dist/angular-bootstrap.js',
       './node_modules/angular-bootstrap-npm/dist/angular-bootstrap-tpls.js',
